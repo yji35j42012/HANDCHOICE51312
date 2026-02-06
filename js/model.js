55483fe6f5186e26863d56fd022238ff4250c3b3
yji35j42012 <yji35j42012@gmail.com>
2026-02-06 13:48:39 +0800

    進度 Front End

--- js/model.js
+++ js/model.js
@@ -85,7 +85,7 @@ model_header.innerHTML = `<div class="container">
 							</li>
 
 							<li class="header-submenu__item">
-								<a class="header-submenu__link" href="https://www.softeq.com/frontend_development">
+								<a class="header-submenu__link" href="frontend_development.html">
 									Front End
 								</a>
 							</li>
@@ -370,7 +370,7 @@ model_mb_menu.innerHTML = `<div class="mb-menu__bar js-menu-box">
                         <li class="mb-menu__submenu-item">
 
                             <a class="mb-menu__submenu-link"
-                                href="https://www.softeq.com/frontend_development">Front End</a>
+                                href="frontend_development.html">Front End</a>
 
                         </li>
 
